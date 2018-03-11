a0cdc0129518e2dde561c78bf0000e566d7aeeb5
James N <jimbo@nicholl.org.uk>
2018-03-11 15:21:08 +0000

    imported files from point palmerson project

---
+++ Scripts/Character.cs
@@ -0,0 +1,26 @@
+﻿using DSA.Extensions.Base;
+
+namespace DSA.Extensions.Characters
+{
+	//NPC class
+	[System.Serializable]
+	public class Character : TraitedMonoBehaviour, IInteractable
+	{
+		public override ExtensionEnum.Extension Extension { get { return ExtensionEnum.Extension.Character; } }
+
+		//used by the player to interact with game entities
+		public virtual void Interact()
+		{
+			//calls interact method in all interactable traits
+			TraitBase[] traits = GetArray();
+			for (int i = 0; i < traits.Length; i++)
+			{
+				if (traits[i] is IInteractCallable)
+				{
+					IInteractCallable interactCallable = (IInteractCallable)traits[i];
+					interactCallable.CallInteract();
+				}
+			}
+		}
+	}
+}
---
+++ Scripts/CharacterManager.cs
@@ -0,0 +1,28 @@
+﻿using DSA.Extensions.Base;
+
+namespace DSA.Extensions.Characters
+{
+	//Manages NPCs
+	//Will likely be extended later as more NPC functionality added
+	public class CharacterManager : ManagerBase
+	{
+		public override ExtensionEnum.Extension Extension { get { return ExtensionEnum.Extension.Character; } }
+
+		private Character[] characters;
+
+		public override void Initialize()
+		{
+			base.Initialize();
+		}
+
+		//Find all characters and pass them to other managers as a traited object
+		public override void Load()
+		{
+			characters = FindObjectsOfType<Character>();
+			for (int i = 0; i < characters.Length; i++)
+			{
+				RaiseTraitsFound(characters[i]);
+			}
+		}
+	}
+}
